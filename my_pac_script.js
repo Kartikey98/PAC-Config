5bc2935b97fb887be8a9065d9a9cdf40a7970b3e
Kartikey98 <56554993+Kartikey98@users.noreply.github.com>
2020-10-01 09:13:26 +0530

    Added Config

---
+++ my_pac_script.js
@@ -0,0 +1,14 @@
+function FindProxyForURL(url, host) {
+
+  var proxy_yes = "PROXY 15.207.247.62:3003";
+  var proxy_no = "DIRECT";
+// If the hostname matches, send to proxy.
+if (shExpMatch(url, "*.zoho.com/*")){
+    return proxy_no;
+}
+// DEFAULT RULE: All other traffic, send direct
+else{
+    return proxy_yes;
+}
+
+}
